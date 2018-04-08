20ad1907dd2c549971a8d5d8e7f38fb8c5963046
xucrt <xuchuanru@gmail.com>
2018-04-09 00:19:26 +0900

    提交2018/04/09

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^post_list/$', views.post_list, name='post_list'),
+    url(r'^post/', views.post, name='post'),
+    #url(r'^post/(\d+)/', views.post, name='post'),
+    #url(r'^post/(?P<post_id>\d+)/', views.post, name='post'),
     url(r'^detail/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,21 @@ from django.http import HttpResponse
 from blog import models
 
 # Create your views here.
-def index(request):
-    articles = models.Article.objects.all()
-    return render(request, 'blog/index.html!', {'blog_list':articles})
+def post_list(request):
+    #print(request.META)
+    return HttpResponse('This is a post list.')
+
+def post(request):
+    print(request.GET)
+    pid = request.GET.get('id', '000')
+    #print(pid)
+    #return HttpResponse('This is a post %s' % post_id)
+    #return HttpResponse('This is a post %s' % pid)
+    #articles = models.Article.objects.all()
+    #return render(request, 'blog/post.html')
+    #return render(request, 'blog/post.html', {'id':pid})
+    post = models.Post.objects.get(id=pid)
+    return render(request, 'blog/post.html', {'post':post})
 
 def detail(request):
     return HttpResponse('这是文章详情')
